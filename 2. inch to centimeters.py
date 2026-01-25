0441788930309ed49575605b2ce2ea595f61885a
safalkharel7 <safalkharel7@gmail.com>
2026-01-25 13:14:36 +0545

    commit

---
+++ 2. inch to centimeters.py
@@ -0,0 +1,6 @@
+inch = input("Enter an inch value: ")
+inch = float(inch)
+if inch > 0:
+    centimeters = inch * 2.54
+    cm = centimeters
+    print(f"{inch} inches is equivalent to {cm:0.2f}centimeters.")
