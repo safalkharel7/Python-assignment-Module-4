77f5761fe6823de1cf92e3ed4960a3713ba4be9e
Dell <safalkharel7@gmail.com>
2026-03-24 23:12:19 +0545

    final

--- 5. username password.py
+++ 5. username password.py
@@ -1,14 +1,13 @@
 username = "python"
 password = "rules"
-attempts = 5                                       #total attempts
-while attempts > 0:                                #as long as there is attempt left
-    input1 = input("\nplease enter your username: ")
-    input2 = input("please enter your password: ")
-    if username == input1 and password == input2:
-        print("\nWelcome")
-        break                                       #otherwise the program will ask for username again
-    else:
-        attempts = attempts - 1
-        print(f"\n You have {attempts} attempts left")   #tells left attempts
-else:                                                    #if the attempt is equal to 0
+input1 = input("\nplease enter your username: ")
+input2 = input("please enter your password: ")
+attempts = 0
+if input1 == username and input2 == password:
+    print("\nWelcome")
+else:
+    while attempts < 4:     #as long as there is attempt left
+        input1 = input("\nplease enter your username again: ")
+        input2 = input("please enter your password again: ")
+        attempts = attempts + 1
     print("\nAccess Denied")
--- 6. approximate value of pi.py
+++ 6. approximate value of pi.py
@@ -1,5 +1,12 @@
-import math
-N = int(input("Enter the number of random points to generate: "))
-n = str(math.pi / 4)
-x = 4 * n / N
-print(x)
+import random
+N = int(input("\nEnter the number of random points to generate: "))
+i = 0
+n = 0
+while i <N:
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+    if x**2+y**2<1:
+        n +=1
+    i +=1
+pi_est = 4*n/N
+print(f"\nThe approx value of pi based on number of random points you entered is: {pi_est}")
